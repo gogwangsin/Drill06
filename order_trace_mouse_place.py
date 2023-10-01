987e43f33e8faf2201f1fa549b7bc661518cf7f0
gogwangsin <gwangsin1029@naver.com>
2023-10-01 17:11:29 +0900

    거리에 따른 랜덤 위치 화살표

--- order_trace_mouse_place.py
+++ order_trace_mouse_place.py
@@ -5,7 +5,8 @@
 # 소년이 도착한 손화살표는 사라짐 ( 1 점 )
 
 from pico2d import *
-
+import random
+import math
 
 TUK_WIDTH, TUK_HEIGHT = 800, 800
 
@@ -28,20 +29,58 @@ def exit_key():
 def Rendering():
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 5, TUK_HEIGHT // 5)
+    boy_draw()
+    arrow_draw()
     update_canvas()
     pass
 
 def Logic():
-    pass
+    global x1, y1, x2, y2
+    t = 0.03  
+    x1 += (x2 - x1) * t
+    y1 += (y2 - y1) * t
+    calculate_distance()
+    set_direction()
+    
+
+def boy_draw():
+    if x_dir < 0:
+        character.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', x1, y1, 100, 100)
+    else:
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+
+def arrow_draw():
+    arrow.draw(x2,y2)
 
 
+def set_direction():
+    global x_dir
+    if x2 - x1 >= 0:
+        x_dir = 1
+    elif x2 - x1 < 0:
+        x_dir = -1
+
+def calculate_distance():
+    global x2, y2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    if distance <= 10:
+        x2 = random.randint(0, TUK_WIDTH - 1)
+        y2 = random.randint(0, TUK_HEIGHT - 1)
+
+
+#===================================================  
+frame = 0
+x1, y1 = TUK_WIDTH, TUK_HEIGHT
+x2 = random.randint(0, TUK_WIDTH - 1)
+y2 = random.randint(0, TUK_HEIGHT - 1)
+x_dir = 1
 while running:
     exit_key()
     if not running:
         break
     Rendering()
     Logic()
-    # frame = ( frame + 1 ) % 8
+    frame = ( frame + 1 ) % 8
     delay(0.05)
 
 close_canvas()
